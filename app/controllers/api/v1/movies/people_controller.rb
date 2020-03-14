d8e1db4fbac4166cf8f9df07e013c4580cf16bd0
Gonzalo <gonzalo.rosendo@aero.tur.ar>
2020-03-14 11:57:35 -0300

    refactoring controllers

--- app/controllers/api/v1/movies/people_controller.rb
+++ app/controllers/api/v1/movies/people_controller.rb
@@ -2,7 +2,6 @@ module Api
   module V1
     module Movies
       class PeopleController < ApplicationController
-        skip_before_action :verify_authenticity_token
 
         def index
           render json: Api::V1::People::Index.new(people_params).call
--- app/controllers/api/v1/people/movies_controller.rb
+++ app/controllers/api/v1/people/movies_controller.rb
@@ -2,7 +2,6 @@ module Api
   module V1
     module People
       class MoviesController < ApplicationController
-        skip_before_action :verify_authenticity_token
 
         def index
           render json: Api::V1::Movies::Index.new(movie_params).call
